be9ebdaddbf0489c75cd0802e1d91cf0f72f9f99
Narcisse TONI <narcissetoni@gmail.com>
2026-02-12 19:37:57 +0100

    Force fallback port 8080

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const http = require("http");
 
+const PORT = process.env.PORT || 8080;
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("SIRBA fonctionne !");
+  res.end("SIRBA SERVER WORKING");
 });
 
-const PORT = process.env.PORT;
-
 server.listen(PORT, "0.0.0.0", () => {
-  console.log(`🚀 Server running on ${PORT}`);
+  console.log("Server running on port", PORT);
 });
